ebcb47d015460d450050026785cfdffa857943c3
mkraja826 <37881042+mkraja826@users.noreply.github.com>
2026-08-24 09:34:41 +0530

    Load ABCR mobile polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./interior.css";
 import "./accessibility.css";
 import "./project-detail.css";
 import "./brand.css";
+import "./mobile-polish.css";
 import VisitorTracker from "@/components/VisitorTracker";
 import { company, contact } from "@/lib/site-data";
 
